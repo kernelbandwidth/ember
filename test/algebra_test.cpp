31d92dccf98503f20bdcab13f86c2c9aefdad6a2
James Crooks <crooks1379@gmail.com>
2025-01-10 22:26:13 -0800

    tests

--- test/algebra_test.cpp
+++ test/algebra_test.cpp
@@ -7,15 +7,36 @@
 TEST_MAIN()
 
 namespace ember {
-TEST_CASE(scalar_multiplication_works) {
-    Tensor t(std::vector({size_t(2), size_t(2)}));
-    for (size_t i = 0; i < t.data.size(); ++i) {
-        t.data[i] = 1;
-    }
 
+void assign_all(Tensor& t, float value) {
+    for (size_t i = 0; i < t.data.size(); ++i) {
+        t.data[i] = value;
+    }
+}
+
+TEST_CASE(assign_all_works) {
+    Tensor t(std::vector({2ul, 2ul}));
+    assign_all(t, 3.0f);
+    for (size_t i = 0; i < t.data.size(); ++i) {
+        EXPECT_EQ(t.data[i], 3.0f);
+    }
+}
+
+TEST_CASE(scalar_multiplication_works) {
+    Tensor t(std::vector({2ul, 2ul}));
+    assign_all(t, 1.0f);
     auto got = alg::sm(t, 3.0f);
     for (size_t i = 0; i < t.data.size(); ++i) {
         EXPECT_AP_EQ(got.data[i], 3.0f);
+    }
+}
+
+TEST_CASE(scalar_multiplication_mut_works) {
+    Tensor t(std::vector({2ul, 2ul}));
+    assign_all(t, 1.0f);
+    alg::sm_mut(t, 3.0f);
+    for (size_t i = 0; i < t.data.size(); ++i) {
+        EXPECT_AP_EQ(t.data[i], 3.0f);
     }
 }
 } // namespace ember
